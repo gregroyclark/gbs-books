9e8713e555a055fa4156eb9b009035ddabf35e09
peacefrog <gregroyclark@gmail.com>
2020-07-11 17:42:04 -0500

    makes image & link required

--- models/book.js
+++ models/book.js
@@ -5,8 +5,8 @@ const bookSchema = new Schema({
   title: { type: String, required: true },
   authors: { type: String, required: true},
   description: { type: String, required: true},
-  image: { },
-  link: { }
+  image: { required: true },
+  link: { required: true }
 });
 
 const Book = mongoose.model("Book", bookSchema);
