b83b52bea1040b61f43d5a7868b7e786e40ee6de
peacefrog <gregroyclark@gmail.com>
2020-07-25 23:37:57 -0500

    runs build

---
+++ client/build/precache-manifest.6a8832ca4d783e21feeb9bc3cf6bbeb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad1692690e3ed2ef26bb947835daade3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc9d07e28c368a0eb330",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "d8cc4f002564ebdaa9bf",
+    "url": "/static/js/2.dab2960d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dab2960d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc9d07e28c368a0eb330",
+    "url": "/static/js/main.8000ea1a.chunk.js"
+  },
+  {
+    "revision": "dfacdea51f04b0cad740",
+    "url": "/static/js/runtime-main.b1f173d4.js"
+  }
+]);
