359221a297209cf1ef3cca9c43d63dd3bf6722b0
peacefrog <gregroyclark@gmail.com>
2020-08-01 13:54:07 -0500

    adds findAll to booksController

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,5 +1,11 @@
 const db = require("../models");
 
 module.exports = {
-
+    findAll: function(req, res) {
+      db.Book
+        .find(req.query)
+        .sort({})
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    }
 };
