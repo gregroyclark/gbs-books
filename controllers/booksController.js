0b34fe2733833513b107d0b9a26c2899cd018328
peacefrog <gregroyclark@gmail.com>
2020-08-03 22:04:08 -0500

    adds date to .sort

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -4,7 +4,7 @@ module.exports = {
     findAll: function(req, res) {
       db.Book
         .find(req.query)
-        .sort({})
+        .sort({ date: -1 })
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     }
